3fa98475d73c10106f3cd984d5ea535f12cd284b
Andrew <61470747+andrew32790@users.noreply.github.com>
2020-06-03 17:15:00 +0300

    Add files via upload

---
+++ Vehiculeclass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_TEST
+{
+   public class Vehiculeclass
+    {
+        public int id { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string An { get; set; }
+
+
+        public string Numar { get; set; }
+
+
+
+        public string FullInfo
+        {
+            get
+            {
+                return $"{Brand} {Model} {An} {Numar} ";
+
+            }
+
+        }
+
+    }
+}
